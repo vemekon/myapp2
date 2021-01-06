93636f2491de44d1b90cd21b6f93cb04e69e973e
samuel112 <senaymokie@gmail.com>
2021-01-06 02:02:36 +0000

    c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import { A } from "./A";
+import { B } from "./B";
+import { M } from "./M";
 
 function App() {
-  return <div className="App">gfdgd</div>;
+  return (
+    <BrowserRouter className="App">
+      <Route path="/a" exact component={A} />
+      <Route path="/b" exact component={B} />
+      <Route path="/" component={M} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/M.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const M = () => {
+  return (
+    <div>
+      Main Main
+      <Link to="/a">AAAA</Link>
+      <br />
+      <Link to="/b">BBBB</Link>
+    </div>
+  );
+};
